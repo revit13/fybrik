c7e0a069422de9f4e1fbdcdee7631fc72fb6005b
Revital Sur <eres@il.ibm.com>
2021-08-08 06:45:06 +0300

    Smart Blueprint: Updated plotter structure (#735)
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- manager/apis/app/v1alpha1/datacatalog_types.go
+++ manager/apis/app/v1alpha1/datacatalog_types.go
@@ -5,11 +5,32 @@ package v1alpha1
 
 import "fybrik.io/fybrik/pkg/serde"
 
-// DataStore contains the details for accesing the data that are sent by catalog connectors
+// Credentials holds the credentials for accessing the data
 // Credentials for accesing the data are stored in Vault, in the location represented by Vault property.
-type DataStore struct {
-	// Holds details for retrieving credentials by the modules from Vault store.
+type Credentials struct {
+	// +required
 	Vault Vault `json:"vault"`
+}
+
+// Credentials holds the credentials for accessing the data for read, write and copy operation types
+type DataStoreCredentials struct {
+	// Read holds the credentials for reading the asset
+	// +optional
+	Read *Credentials `json:"read,omitempty"`
+
+	// Write holds the credentials for writing the asset
+	// +optional
+	Write *Credentials `json:"write,omitempty"`
+
+	// Copy holds the credentials for copying the asset
+	// +optional
+	Copy *Credentials `json:"copy,omitempty"`
+}
+
+// DataStore contains the details for accesing the data that are sent by catalog connectors
+type DataStore struct {
+	// +required
+	Credentials DataStoreCredentials `json:"credentials"`
 	// Connection has the relevant details for accesing the data (url, table, ssl, etc.)
 	// +required
 	Connection serde.Arbitrary `json:"connection"`
--- manager/apis/app/v1alpha1/plotter_types.go
+++ manager/apis/app/v1alpha1/plotter_types.go
@@ -7,18 +7,251 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// AssetDetails is a list of assets used in the fybrikapplication. In addition to assets declared in
+// fybrikapplication, AssetDetails list also contains assets that are allocated by the control-plane
+// in order to serve fybrikapplication
+type AssetDetails struct {
+
+	// AssetID identifies the asset to be used for accessing the data
+	// +required
+	AssetID string `json:"assetID"`
+
+	// AdvertisedAssetID links this asset to asset from fybrikapplication and is used by user facing services
+	// +optional
+	AdvertisedAssetID string `json:"advertisedAssetID,omitempty"`
+
+	// Cluster name where the asset resides
+	// +required
+	ClusterName string `json:"cluster"`
+
+	// +required
+	DataStore DataStore `json:"assetDetails"`
+}
+
+type Service struct {
+	// AssetID which this flow process
+	// +required
+	AssetID string `json:"assetID"`
+	// Interface indicates the protocol and format expected by the data user
+	// +required
+	Interface InterfaceDetails `json:"interface"`
+}
+
+type StepAPI struct {
+	// +required
+	Service Service `json:"service"`
+}
+
+// StepSource is the source of this step: it could be assetID
+// or the sink of another step
+type StepSource struct {
+	// AssetID identifies the source asset of this step
+	// +required
+	AssetID string `json:"assetID"`
+
+	// API indicates to the application how to access the module for the given capability
+	// +optional
+	API *StepAPI `json:"api,omitempty"`
+}
+
+// StepSink holds information to where the target data will be written:
+// it could be assetID of an asset specified in fybrikapplication or an an asset created
+// by fybrik control-plane
+type StepSink struct {
+	// AssetID identifies the target asset of this step
+	// +required
+	AssetID string `json:"assetID"`
+}
+
+// StepParameters contains the parameters to the module
+// that is deployed in this step
+type StepParameters struct {
+	// +optional
+	Source *StepSource `json:"source,omitempty"`
+
+	// +optional
+	Sink *StepSink `json:"sink,omitempty"`
+
+	// API indicates to the application how to access the module for the given capability
+	// +optional
+	API *StepAPI `json:"api,omitempty"`
+
+	// Actions are the data transformations that the module supports
+	// +optional
+	Actions []SupportedAction `json:"action,omitempty"`
+}
+
+// Step contains details of a data flow step
+type Step struct {
+	// Name of the step
+	// +required
+	Name string `json:"name"`
+
+	// Name of the cluster this step is executed on
+	// +required
+	Cluster string `json:"cluster"`
+
+	// The template name of this step.
+	// The full details of the template can be extracted from Plotter.spec.templates
+	// list field.
+	// +required
+	Template string `json:"template"`
+
+	// +optional
+	Parameters *StepParameters `json:"parameters,omitempty"`
+}
+
+// SequentialSteps defines a series of sequential data flow steps
+// +kubebuilder:validation:Type=array
+type SequentialSteps struct {
+	// +required
+	Steps []Step `json:"-" protobuf:"bytes,1,rep,name=steps"`
+}
+
+// Subflows is a list of data flows which are originated from the same data asset
+// but are triggered differently (e.g., one upon init
+// trigger and one upon workload trigger)
+type SubFlow struct {
+	// Name of the SubFlow
+	// +required
+	Name string `json:"name"`
+
+	// Type of the flow (e.g. read)
+	// +required
+	FlowType string `json:"flowType"`
+
+	// Triggers
+	// +required
+	Triggers []CapabilityScope `json:"triggers"`
+
+	// Steps defines a series of sequential/parallel data flow steps
+	// +required
+	Steps []SequentialSteps `json:"steps" protobuf:"bytes,11,opt,name=steps"`
+}
+
+// Flows is the list of data flows driven from fybrikapplication:
+// Each element in the list holds the flow of the data requested in fybrikapplication.
+type Flow struct {
+	// Name of the flow
+	// +required
+	Name string `json:"name"`
+
+	// Type of the flow (e.g. read)
+	// +required
+	FlowType string `json:"flowType"`
+
+	// AssetID of this data flow
+	// +required
+	AssetID string `json:"assetID"`
+
+	// +required
+	SubFlows []SubFlow `json:"subFlows"`
+}
+
+// Basic info needed for chart deployment
+type ChartBasicInfo struct {
+	// Name of the chart
+	// +required
+	Name string `json:"name"`
+}
+
+// ModuleBasicInfo is a subset of M4DModule Custom Resource.  It contains partial information
+// to instantiate resource of type M4DModule. The complete information to instantiate the module
+// can be constructed from other properties in the plotter e.g., the flows
+type ModuleBasicInfo struct {
+	// Name of the template
+	// +required
+	Name string `json:"name"`
+
+	// Kind of k8s resource
+	// +required
+	Kind string `json:"kind"`
+
+	// +required
+	Chart ChartBasicInfo `json:"chart"`
+}
+
+// Template contains basic information about the required modules to serve the fybrikapplication
+type Template struct {
+	// Template name
+	Name string `json:"name"`
+	// Modules is a list of dependent modules. e.g., if a plugin module is used
+	// then the service module is used in should appear in the same modules list.
+	// +required
+	Modules []ModuleBasicInfo `json:"modules"`
+}
+
 // PlotterSpec defines the desired state of Plotter, which is applied in a multi-clustered environment. Plotter installs the runtime environment
 // (as blueprints running on remote clusters) which provides the Data Scientist's application with secure and governed access to the data requested in the
 // FybrikApplication.
 type PlotterSpec struct {
 
 	// Selector enables to connect the resource to the application
-	// Should match the selector of the owner - FybrikApplication CRD.
-	Selector metav1.LabelSelector `json:"selector"`
+	// Application labels should match the labels in the selector.
+	// For some flows the selector may not be used.
+	// +optional
+	Selector Selector `json:"appSelector,omitempty"`
 
 	// +required
-	// Blueprints structure represents remote blueprints mapped by the identifier of a cluster in which they will be running
-	Blueprints map[string]BlueprintSpec `json:"blueprints"`
+	Assets []AssetDetails `json:"assets"`
+
+	// +required
+	Flows []Flow `json:"flows"`
+
+	// +required
+	Templates []Template `json:"templates"`
+}
+
+// SubFlowsStatus holds status information about a subFlow
+type SubFlowsStatus struct {
+	// SubFlow name
+	// +required
+	Name string `json:"name"`
+
+	// ObservedState includes information about this subflow
+	// It includes readiness and error indications, as well as user instructions
+	// +optional
+	ObservedState ObservedState `json:"status,omitempty"`
+}
+
+// AssetStatus includes information to be reported back to the FybrikApplication resource
+// It holds the status and access information for each asset in fybrikapplication
+type AssetStatus struct {
+	// Asset name
+	// +required
+	Name string `json:"name"`
+
+	// Endpoint
+	// +required
+	Endpoint string `json:"endpoint"`
+
+	// Port
+	// +required
+	Port int32 `json:"port"`
+
+	// ObservedState includes information to be reported back to the FybrikApplication resource
+	// It includes readiness and error indications, as well as user instructions
+	// +optional
+	ObservedState ObservedState `json:"status,omitempty"`
+
+	// +optional
+	Errors []string `json:"errors,omitempty,omitempty"`
+}
+
+// FlowStatus includes information to be reported back to the FybrikApplication resource
+// It holds the status per data flow
+type FlowStatus struct {
+	// Template name
+	// +required
+	Name string `json:"name"`
+
+	// ObservedState includes information about the current flow
+	// It includes readiness and error indications, as well as user instructions
+	// +optional
+	ObservedState ObservedState `json:"status,omitempty"`
+
+	// +required
+	SubFlows []SubFlowsStatus `json:"subFlows"`
 }
 
 // PlotterStatus defines the observed state of Plotter
@@ -35,11 +268,15 @@ type PlotterStatus struct {
 	// +optional
 	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 
-	// + optional
-	Blueprints map[string]MetaBlueprint `json:"blueprints,omitempty"`
+	// +required
+	Flows []FlowStatus `json:"flows"`
 
-	// + optional
-	ReadyTimestamp *metav1.Time `json:"readyTimestamp,omitempty"`
+	// +required
+	Assets []AssetStatus `json:"assets"`
+
+	// Conditions represent the possible error and failure conditions
+	// +optional
+	Conditions []Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true

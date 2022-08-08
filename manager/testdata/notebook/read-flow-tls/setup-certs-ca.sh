144fa18384e5b783eb9aabfc57157b0dca5c2251
Revital Sur <eres@il.ibm.com>
2022-08-08 07:56:45 +0300

    test

--- manager/testdata/notebook/read-flow-tls/setup-certs-ca.sh
+++ manager/testdata/notebook/read-flow-tls/setup-certs-ca.sh
@@ -2,12 +2,21 @@
 # Copyright 2021 IBM Corp.
 # SPDX-License-Identifier: Apache-2.0
 
+ROOT_DIR := ../../../../
+
 # Apply certificates for tls authentication
 kubectl -n fybrik-system apply -f ca-certificate.yaml
 kubectl -n fybrik-system apply -f katalog-connector-certificates.yaml
-kubectl -n fybrik-system apply -f opa-server-certificates.yaml
 kubectl -n fybrik-system apply -f opa-connector-certificates.yaml
 kubectl -n fybrik-system apply -f manager-certificates.yaml
+
+kubectl get secret test-tls-ca-certs -o json | jq -r '.data."ca.crt"' | base64 -d > /tmp/ca.crt
+
+cp katalog-connector-Dockerfile ${ROOT_DIR}/connectors/katalog/Dockerfile
+cp opa-connector-Dockerfile ${ROOT_DIR}/connectors/opa/Dockerfile
+cp manager-Dockerfile ${ROOT_DIR}/manager/Dockerfile
+
+
 
 
 

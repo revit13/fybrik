cf5237d1afb55e8c136fe1590965532e70013dc6
Revital Sur <eres@il.ibm.com>
2022-11-28 08:58:22 +0000

    Change TLS test to work with openmetadata
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- manager/testdata/notebook/read-flow-tls/setup-certs.sh
+++ manager/testdata/notebook/read-flow-tls/setup-certs.sh
@@ -6,12 +6,15 @@ FYBRIK_NAMESPACE=fybrik-system
 
 kubectl create ns ${FYBRIK_NAMESPACE}
 # Apply certificates for tls authentication
-kubectl -n ${FYBRIK_NAMESPACE} apply -f ca-certificate.yaml
-kubectl -n ${FYBRIK_NAMESPACE} apply -f katalog-connector-certificates.yaml
-kubectl -n ${FYBRIK_NAMESPACE} apply -f opa-server-certificates.yaml
-kubectl -n ${FYBRIK_NAMESPACE} apply -f opa-connector-certificates.yaml
-kubectl -n ${FYBRIK_NAMESPACE} apply -f manager-certificates.yaml
-kubectl -n ${FYBRIK_NAMESPACE} apply -f vault-certificates.yaml
-kubectl -n ${FYBRIK_NAMESPACE} apply -f arrow-flight-module-certificates.yaml
+kubectl -n ${FYBRIK_NAMESPACE} apply -f ca-certificate.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f openmetadata-connector-certificates.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f opa-server-certificates.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f opa-connector-certificates.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f manager-certificates.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f vault-certificates.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f arrow-flight-module-certificates.yaml --wait
 
 
+
+kubectl get secret test-tls-openmetadata-connector-certs -n fybrik-system -o json | jq -r '.data."tls.crt"' | base64 -d > tls.crt
+docker run -it -v ${PWD}:/local joostdecock/keytool -import -alias localhost -file /local/tls.crt -storepass test12 -keystore /local/openmetadata.keystore.jks -noprompt 

60623c14c0fe92c0398ce7e50bdd056991fe4672
Revital Sur <eres@il.ibm.com>
2022-11-28 13:35:02 +0000

    Fixes.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- manager/testdata/notebook/read-flow-tls/setup-certs.sh
+++ manager/testdata/notebook/read-flow-tls/setup-certs.sh
@@ -13,8 +13,10 @@ kubectl -n ${FYBRIK_NAMESPACE} apply -f opa-connector-certificates.yaml --wait
 kubectl -n ${FYBRIK_NAMESPACE} apply -f manager-certificates.yaml --wait
 kubectl -n ${FYBRIK_NAMESPACE} apply -f vault-certificates.yaml --wait
 kubectl -n ${FYBRIK_NAMESPACE} apply -f arrow-flight-module-certificates.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f localhost-certificates.yaml --wait
 
 
-
+while ! kubectl get secret test-tls-openmetadata-connector-certs --namespace fybrik-system; do echo "Waiting for my secret. CTRL-C to exit."; sleep 1; done
+rm -f openmetadata.keystore.jks || true
 kubectl get secret test-tls-openmetadata-connector-certs -n fybrik-system -o json | jq -r '.data."tls.crt"' | base64 -d > tls.crt
 docker run -it -v ${PWD}:/local joostdecock/keytool -import -alias localhost -file /local/tls.crt -storepass test12 -keystore /local/openmetadata.keystore.jks -noprompt 

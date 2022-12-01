82e378dccf803113cb83d30ee8ffc2a762c498e3
Revital Sur <eres@il.ibm.com>
2022-12-01 07:29:49 +0000

    changes
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- manager/testdata/notebook/read-flow-tls/setup-certs.sh
+++ manager/testdata/notebook/read-flow-tls/setup-certs.sh
@@ -14,10 +14,11 @@ kubectl -n ${FYBRIK_NAMESPACE} apply -f manager-certificates.yaml --wait
 kubectl -n ${FYBRIK_NAMESPACE} apply -f vault-certificates.yaml --wait
 kubectl -n ${FYBRIK_NAMESPACE} apply -f arrow-flight-module-certificates.yaml --wait
 kubectl -n ${FYBRIK_NAMESPACE} apply -f localhost-certificates.yaml --wait
+kubectl -n ${FYBRIK_NAMESPACE} apply -f openmetadata-certificates.yaml --wait
 
 # Prepare cert for openmetadata deployment
 # ref: https://docs.open-metadata.org/deployment/security/enable-ssl/openmetadata-server#enable-ssl-at-the-openmetadata-server
-while ! kubectl get secret test-tls-openmetadata-connector-certs --namespace fybrik-system; do echo "Waiting for my secret. CTRL-C to exit."; sleep 1; done
+while ! kubectl get secret test-tls-openmetadata-certs --namespace fybrik-system; do echo "Waiting for my secret. CTRL-C to exit."; sleep 1; done
 rm -f openmetadata.keystore.jks || true
 kubectl get secret test-tls-openmetadata-connector-certs -n fybrik-system -o json | jq -r '.data."tls.crt"' | base64 -d > tls.crt
 docker run -it -v ${PWD}:/local joostdecock/keytool -import -alias localhost -file /local/tls.crt -storepass test12 -keystore /local/openmetadata.keystore.jks -noprompt

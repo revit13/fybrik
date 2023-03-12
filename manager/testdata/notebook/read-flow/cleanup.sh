bf15e1866ca3f8b2578fe48c41ad610c66fa9edb
Revital Sur <eres@il.ibm.com>
2023-03-12 11:53:34 +0200

    Changes.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- manager/testdata/notebook/read-flow/cleanup.sh
+++ manager/testdata/notebook/read-flow/cleanup.sh
@@ -7,13 +7,6 @@ set -x
 kubectl delete namespace fybrik-notebook-sample || true
 NS="fybrik-system"; kubectl -n $NS get configmap | awk '/sample/{print $1}' | xargs  kubectl delete -n $NS configmap
 
-kubectl port-forward svc/openmetadata-connector -n fybrik-system $local_port:$port &
-
-curl -X POST localhost:8080/getAssetInfo -d '{"assetID": "AssetID", "operationType": "read"}' -H "Content-type: application/json"   -H "X-Request-Datacatalog-Cred: QQQ"
-
-# Create asset and secret
-kubectl -n fybrik-notebook-sample apply -f s3credentials.yaml
-
 if [[ "${USE_OPENMETADATA_CATALOG}" -eq 1 ]]; then
   port=8080
   local_port=8081
@@ -33,7 +26,7 @@ if [[ "${USE_OPENMETADATA_CATALOG}" -eq 1 ]]; then
   # Wait until curl command succeed
   c=0
   # -k flag is used to skip server verification to avoid errors regarding target host name 'localhost'
-  while [[ $(curl -X POST $certs $prefix:localhost:${local_port}/getAssetInfo -d '{"assetID": ${CATALOGED_ASSET}, "operationType": "read"}' -H "Content-type: application/json"   -H "X-Request-Datacatalog-Cred: QQQ") != *'assetID'* ]]
+  while [[ $(curl $certs -k -X POST $prefix:localhost:${local_port}/getAssetInfo -d '{"assetID": ${CATALOGED_ASSET}, "operationType": "read"}' -H "Content-type: application/json"   -H "X-Request-Datacatalog-Cred: QQQ") != *'assetID'* ]]
   do
     echo "waiting for curl command to createAsset to succeed"
     ((c++)) && ((c==25)) && break
@@ -41,25 +34,4 @@ if [[ "${USE_OPENMETADATA_CATALOG}" -eq 1 ]]; then
   done
 fi
 
-# Avoid using webhooks in tests
-kubectl delete validatingwebhookconfiguration fybrik-system-validating-webhook
-
-if [[ -z "${LATEST_BACKWARD_SUPPORTED_AFM_VERSION}" ]]; then
-  # Use master version of arrow-flight-module according to https://github.com/fybrik/arrow-flight-module#version-compatbility-matrix
-  kubectl apply -f https://raw.githubusercontent.com/fybrik/arrow-flight-module/master/module.yaml -n fybrik-system
-else
-  kubectl apply -f https://github.com/fybrik/arrow-flight-module/releases/download/${LATEST_BACKWARD_SUPPORTED_AFM_VERSION}/module.yaml -n fybrik-system
-fi
-
-# When Vault uses mutual TLS the certificates and private key for the arrow-flight-module
-# are stored in a secret in fybrik-system namespace and copied to fybrik-blueprint namespace
-# using a mechanism for syncing secrets across namespaces.
-# ref: https://cert-manager.io/docs/tutorials/syncing-secrets-across-namespaces
-if ! [[ -z "$PATCH_FYBRIK_MODULE" ]]; then
-  # Patch FybrikModule to use the secret
-  kubectl patch fybrikmodules.app.fybrik.io arrow-flight-module -n fybrik-system -p "{\"spec\": {\"chart\":{\"values\":{\"tls.certs.cacertSecretName\":\"test-tls-arrow-flight-certs\"}}}}" --type="merge"
-  kubectl patch fybrikmodules.app.fybrik.io arrow-flight-module -n fybrik-system -p "{\"spec\": {\"chart\":{\"values\":{\"tls.certs.certSecretName\":\"test-tls-arrow-flight-certs\"}}}}" --type="merge"
-fi
-
-# Forward port of test S3 instance
-kubectl port-forward -n fybrik-system svc/s3 9090:9090 &
+aws --endpoint-url=http://localhost:9090 s3api --bucket=bucket1 delete-objects --delete='{"Objects": [{"Key": "data.csv"}]}'
